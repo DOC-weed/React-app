bc7e6c7216f62ce21c731052e9d08fc0e7e1cefa
EDMO19 <erikmendozaolivares@gmail.com>
2021-06-15 20:30:57 -0500

    actualizacion footer/Erik

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../assets/css/FooterComponent.css';
 import logo from '../assets/images/logo.png';
+import kawaii from '../assets/images/kawaii.png'
 
 class FooterComponent extends React.Component {
     render() {
         return ( 
             <div className="container_footer">
+                <div className="kawaii-container">
+                    <img src={kawaii} />
+                </div>
                 <div className="img-container">
                     <img className="img" src={logo} alt="" />
                 </div>
