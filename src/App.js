4c848c956b3168b8aa042feb7de674fd4af5c97c
DOC-weed <andres@MacBook-Air-de-CEOS-6.local>
2021-06-29 21:03:29 -0500

    loginCompnent/AndresLomeli

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
 import Login from './pages/login';
 import Home from './pages/Home';
+import HeaderComponent from './components/HeaderComponent';
+
 import {BrowserRouter as Router, Redirect,Route, Switch } from 'react-router-dom';
 
 function App() {
+  
   return (
-   <Router>
-     <Switch>
-       <Route path="/home">
-         <Home/> 
-       </Route>
-       <Route path="/">
-         <Login/>
-       </Route>
-     </Switch>
-   </Router>
+    <>
+    <HeaderComponent />
+    <Login></Login>
+    </>
   );
 }
 
