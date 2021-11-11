9149b91f1dce5cf787290684eaf3165ae8100e3e
DJmaca <aleesfeo1234@gmail.com>
2021-11-10 18:16:29 -0600

    sw

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from '../public/service-worker';
 import FooterComponent from './components/FooterComponent';
 
 // Importing the Bootstrap CSS
@@ -23,4 +23,15 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.unregister();
+//
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('./service-worker.js').then(() =>{
+    console.log('CLIENT: service worker registration complete.');
+  }).catch(err=>{
+    console.log('CLIENT: service worker registration failure.');
+    console.log(err);
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
--- src/serviceWorker.js
+++
@@ -1,8 +0,0 @@
-export function unregister(){
-    if('serviceEorker' in navigator){
-        navigator.serviceWorker.ready.then(registration =>{
-            registration.unregister();
-            console.log('unregister');
-        });
-    }
-}
